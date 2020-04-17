f759b0fa249da4352a1bdd77c3c332bfb161aeae
ayush <ayushpande202@gmail.com>
2020-04-17 22:52:05 +0530

    temp.py

--- temp1.py
+++ temp1.py
@@ -13,8 +13,8 @@ for subdir, dirs, files in os.walk('/media/ayush/Elements/Mask_Recognition/Mask-
 print(len(list))
 '''
 import cv2
-img = cv2.imread('/media/ayush/Elements/Mask_Recognition/Mask-Dataset/17. Bollywood-actors-beard-styles-2015.jpg')
-cv2.rectangle(img, (0,84),(150,185), (0, 255, 0), 2)
+img = cv2.imread('/media/ayush/Elements/Mask_Recognition/Mask-Dataset/RWMFD_part_2_pro/00000/test.jpg')
+cv2.rectangle(img, (66,36),(155,126), (0, 255, 0), 2)
 #cv2.rectangle(img, (171,587),(439,854), (0, 255, 0), 2)
 while True:
     cv2.imshow('',img)
